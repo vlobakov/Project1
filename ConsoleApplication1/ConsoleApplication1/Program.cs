d8fb287d8fd357281e0f137c533a6c151af4335c
Vitaliy Lobakov <lobakov@yahoo.com>
2012-06-05 12:12:43 +0300

    bla

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public interface IModifyMyValue
+        {
+            int X
+            {
+                //get;
+                set;
+            }
+        }
+
+        public struct MyValue : IModifyMyValue
+        {
+            public int x;
+
+            public int X
+            {
+                //get
+                //{
+                //    return x;
+                //}
+
+                set
+                {
+                    x = value;
+                }
+            }
+
+            public override string ToString()
+            {
+                System.Text.StringBuilder output =
+                    new System.Text.StringBuilder();
+                output.AppendFormat("{0}", x);
+                return output.ToString();
+            }
+        }
+
+        public class EntryPoint
+        {
+            static void Main()
+            {
+                // Create value
+                MyValue myval = new MyValue();
+                myval.x = 123;
+
+                // box it
+                object obj = myval;
+                System.Console.WriteLine("{0}", obj.ToString());
+
+                // modify the contents in the box.
+                IModifyMyValue iface = (IModifyMyValue)obj;
+                iface.X = 456;
+                System.Console.WriteLine("{0}", obj.ToString());
+
+                // unbox it and see what it is.
+                MyValue newval = (MyValue)obj;
+                System.Console.WriteLine("{0}", newval.ToString());
+                Console.ReadLine();
+            }
+        }
+
+
+    }
+}
