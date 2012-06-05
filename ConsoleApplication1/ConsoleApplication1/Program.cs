da5239bda579a88e13a9a860788ab4636ff27668
vlobakov <lobakov@yahoo.com>
2012-06-05 12:19:39 +0300

    Update master

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         {
             int X
             {
-                //get;
+                //get; my comment
                 set;
             }
         }
